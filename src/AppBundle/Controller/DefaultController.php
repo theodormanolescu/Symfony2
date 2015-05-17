aa8c9cb0952ec007555e3461b1d7b2a51f5fe446
Theodor Manolescu <manolescu.theodor@yahoo.com>
2015-05-17 10:21:11 +0300

    working on locale conversion

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,20 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Intl\Intl;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('default/index.html.twig');
+        $locale = $request->getLocale();
+
+        $currency = Intl::getCurrencyBundle()->getCurrencyName('RON');
+
+        return $this->render('default/index.html.twig',
+            array(
+                'currencies' => $currency
+            ));
     }
 }
